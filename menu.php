5e630a4af6692b0f6faba948c510c616f5f2ca1f
sriharan40 <sriharan40@gmail.com>
2016-11-16 23:50:39 +0530

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,14 @@
+<h3><u>Bot Performance and Analytics</u></h3>
+<ol>
+<li><a href="https://www.dashbot.io/login" target="blank">Dashbot Analytics</a></li>
+</ol>
+
+<h3><u>Bot Training</u></h3>
+<ol>
+<li><a href="https://console.api.ai/api-client/#/login">API AI</a></li>
+</ol>
+<h3><u>Business Cases</u></h3>
+<ol>
+<li><a href="index.php">Top Selling Offers</a></li>
+<li><a href="index.php">Broadcast to FB users</a></li>
+</ol>
