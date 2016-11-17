c5ba155bdd8aef78996852bb0506544e8225ec6d
sriharan40 <sriharan40@gmail.com>
2016-11-17 20:45:55 +0800

    Update edit.php

--- edit.php
+++ edit.php
@@ -2,6 +2,30 @@
 <title>Add Top Offers</title>
 <body style="margin:0 auto;">
 <style>
+ ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    width: 200px;
+    background-color: #f1f1f1;
+}
+
+li a {
+    display: block;
+    color: #000;
+    padding: 8px 16px;
+    text-decoration: none;
+}
+
+li a.active {
+    background-color: #4CAF50;
+    color: white;
+}
+
+li a:hover:not(.active) {
+    background-color: #555;
+    color: white;
+}
  table {
     border-collapse: collapse;
     width: 100%;
