27f08859c0397c161a5d73a3baafd7955a83664c
sriharan40 <sriharan40@gmail.com>
2016-11-17 21:23:06 +0800

    Update menu.php

--- menu.php
+++ menu.php
@@ -29,12 +29,12 @@ li a:hover:not(.active) {
 <li><a href="https://www.dashbot.io/login" target="blank">Dashbot Analytics</a></li>
     </ol></ul>
 
-<h3><u>Bot Training</u></h3>
+<ul><h3>Bot Training</h3>
 <ol>
 <li><a href="https://console.api.ai/api-client/#/login">API AI</a></li>
-</ol>
-<h3><u>Business Cases</u></h3>
+    </ol></ul>
+<ul><h3>Business Cases</h3>
 <ol>
 <li><a href="index.php">Top Selling Offers</a></li>
 <li><a href="index.php">Broadcast to FB users</a></li>
-</ol>
+    </ol></ul>
