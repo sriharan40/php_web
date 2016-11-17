7f09e832f6ae3c3253198babd77b222ad86df601
sriharan40 <sriharan40@gmail.com>
2016-11-17 20:26:00 +0800

    Update edit.php

--- edit.php
+++ edit.php
@@ -2,6 +2,22 @@
 <title>Add Top Offers</title>
 <body style="margin:0 auto;">
 <style>
+ table {
+    border-collapse: collapse;
+    width: 100%;
+}
+
+th, td {
+    text-align: left;
+    padding: 8px;
+}
+
+tr:nth-child(even){background-color: #f2f2f2}
+
+th {
+    background-color: #4CAF50;
+    color: white;
+}
 ol li{	
 padding:5px;
 padding-left:0px;	
