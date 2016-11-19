4cbfc08b5de21d93ea4054126107d077f3762892
sriharan40 <sriharan40@gmail.com>
2016-11-19 12:13:01 +0530

    Update edit.php

--- edit.php
+++ edit.php
@@ -4,52 +4,6 @@ session_start();
 <html>
 <title>Add Top Offers</title>
 <body style="margin:0 auto;">
-<style>
- ul {
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-    width: 200px;
-    background-color: #f1f1f1;
-}
-
-li a {
-    display: block;
-    color: #000;
-    padding: 8px 16px;
-    text-decoration: none;
-}
-
-li a.active {
-    background-color: #4CAF50;
-    color: white;
-}
-
-li a:hover:not(.active) {
-    background-color: #555;
-    color: white;
-}
- table {
-    border-collapse: collapse;
-    width: 100%;
-}
-
-th, td {
-    text-align: left;
-    padding: 8px;
-}
-
-tr:nth-child(even){background-color: #f2f2f2}
-
-th {
-    background-color: #4CAF50;
-    color: white;
-}
-ol li{	
-padding:5px;
-padding-left:0px;	
-}
-</style>
 <?php
 error_reporting(0);
 if ($_SESSION['login'] == "")
@@ -57,12 +11,10 @@ if ($_SESSION['login'] == "")
 	header("location:login.php");	
 }
 ?>
-<div style="float:left; width:200px;  padding-left:20px; border-right:1px solid #333333; height:100%;">
 <?php
 include("menu.php");
 ?>
-</div>
-<div style="float:left; padding-left:20px;">
+<div id="content">
 <h3><u>Add Top Offers</u></h3>
 <br/><br/>
 <form method="POST" action="index.php" name="form1">
