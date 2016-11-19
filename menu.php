a4cb8c43b34dd73d81901401d178140719e0f9d9
sriharan40 <sriharan40@gmail.com>
2016-11-20 01:09:25 +0800

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,32 +1,128 @@
-<link rel="stylesheet" href="bootstrap.min.css" />
+<style>
+/* Navigation Menu - Background */
+.navigation {
+  /* critical sizing and position styles */
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 0;
+  
+  /* non-critical appearance styles */
+  list-style: none;
+  background: #111;
+}
 
-<link rel="stylesheet" href="bootstrap-responsive.min.css" />
+/* Navigation Menu - List items */
+.nav-item {
+  /* non-critical appearance styles */
+  width: 200px;
+  border-top: 1px solid #111;
+  border-bottom: 1px solid #000;
+}
 
-<link rel="stylesheet" href="uniform.css" />
+.nav-item a {
+  /* non-critical appearance styles */
+  display: block;
+  padding: 1em;
+  background: linear-gradient(135deg, rgba(0,0,0,0) 0%,rgba(0,0,0,0.65) 100%);
+  color: white;
+  font-size: 1.2em;
+  text-decoration: none;
+  transition: color 0.2s, background 0.5s;
+}
 
-<link rel="stylesheet" href="select2.css" />
+.nav-item a:hover {
+  color: #c74438;
+  background: linear-gradient(135deg, rgba(0,0,0,0) 0%,rgba(75,20,20,0.65) 100%);
+}
 
-<link rel="stylesheet" href="matrix-style.css" />
+/* Site Wrapper - Everything that isn't navigation */
+.site-wrap {
+  /* Critical position and size styles */
+  min-height: 100%;
+  min-width: 100%;
+  background-color: white; /* Needs a background or else the nav will show through */
+  position: relative;
+  top: 0;
+  bottom: 100%;
+  left: 0;
+  z-index: 1;
+  
+  /* non-critical apperance styles */
+  padding: 4em;
+  background-image: linear-gradient(135deg, rgb(254,255,255) 0%,rgb(221,241,249) 35%,rgb(160,216,239) 100%);
+  background-size: 200%;
+}
 
-<link rel="stylesheet" href="matrix-media.css" />
-<!-- CHANGE for site header and code -->
-<!--Header-part-->
+/* Nav Trigger */
+.nav-trigger {
+  /* critical styles - hide the checkbox input */
+  position: absolute;
+  clip: rect(0, 0, 0, 0);
+}
 
-<div id="header">
+label[for="nav-trigger"] {
+  /* critical positioning styles */
+  position: fixed;
+  left: 15px; top: 15px;
+  z-index: 2;
+  
+  /* non-critical apperance styles */
+  height: 30px;
+  width: 30px;
+  cursor: pointer;
+  background-image: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' version='1.1' x='0px' y='0px' width='30px' height='30px' viewBox='0 0 30 30' enable-background='new 0 0 30 30' xml:space='preserve'><rect width='30' height='6'/><rect y='24' width='30' height='6'/><rect y='12' width='30' height='6'/></svg>");
+  background-size: contain;
+}
 
-  <h1><a href="#">BotControl Room</a></h1>
+/* Make the Magic Happen */
+.nav-trigger + label, .site-wrap {
+  transition: left 0.2s;
+}
 
-</div>
+.nav-trigger:checked + label {
+  left: 215px;
+}
 
+.nav-trigger:checked ~ .site-wrap {
+  left: 200px;
+  box-shadow: 0 0 5px 5px rgba(0,0,0,0.5);
+}
+
+body {
+	/* Without this, the body has excess horizontal scroll when the menu is open */
+  overflow-x: hidden;
+}
+
+/* Additional non-critical styles */
+
+h1, h3, p {
+  max-width: 600px;
+  margin: 0 auto 1em;
+}
+
+code {
+	padding: 2px;
+	background: #ddd;
+}
+
+/* Micro reset */
+*,*:before,*:after{-webkit-box-sizing:border-box;-moz-box-sizing:border-box;box-sizing:border-box;margin:0;padding:0;}
+html, body { height: 100%; width: 100%; font-family: Helvetica, Arial, sans-serif; }
+</style>
 <!--close-Header-part--> 
 
-<div id="sidebar">
-		<ul>
+
+		<ul class="navigation">
 		<li>
 		<a href="#">Bot Analytics</a>
 		<ul>
 			<li>
-				<li><a href="https://www.dashbot.io/login">Dashbot Analytics</a>         
+				<li class ="nav-item"><a href="https://www.dashbot.io/login">Dashbot Analytics</a>         
 			</li>
 			
 		</ul>
@@ -34,7 +130,7 @@
 	<li>
 		<a href="#">Training Bot</a>
 		<ul>
-			<li>
+			<li class = "nav-item">
 				<a href="https://console.api.ai/api-client/#/login">API.AI</a>
 			</li>
 			
@@ -43,21 +139,22 @@
 	<li>
 		<a href="#">Business Cases</a>
 		<ul>
-			 <li><a href="index.php">Top Selling Offers</a></li>
-			 <li><a href="fb_campaign.php">Broadcast to FB users</a></li>
+			 <li class = "nav-item"><a href="index.php">Top Selling Offers</a></li>
+			 <li class = "nav-item"><a href="fb_campaign.php">Broadcast to FB users</a></li>
 		</ul>
 	<li>
 	<li>
 		<a href="#">Controls</a>
 		<ul>
-			<li>
+			<li class = "nav-item">
 				<a href="logout.php">Sign Out</a>
 			</li>
 			
 		</ul>
 	<li>
 </ul>
-</div>
+<input type="checkbox" id="nav-trigger" class="nav-trigger" />
+<label for="nav-trigger"></label>
 		<!--<div id="body" class="clear">
 			<div id="sidebar" class="column-left">
 			<ul>
