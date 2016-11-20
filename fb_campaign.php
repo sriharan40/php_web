745d8ea92529c33d7c4c2a39b8525ce90e84a66c
sriharan40 <sriharan40@gmail.com>
2016-11-20 19:36:42 +0530

    Update fb_campaign.php

--- fb_campaign.php
+++ fb_campaign.php
@@ -3,6 +3,14 @@ session_start();
 ?>
 <html>
 <title>Fb Campaign Message</title>
+<script src="jquery-1.12.4.js"></script>
+<script>
+$(document).ready(function (){
+$(".add_option").click(function (){
+$( ".template" ).clone().removeClass("template").show().appendTo( ".fb_content" );	
+});
+});
+</script>
 <body style="margin:0 auto;">
 <?php
 error_reporting(0);
@@ -17,23 +25,24 @@ include("menu.php");
 <br />
 <?php
 $message = $_POST["message"];
-$option1 = $_POST["option1"];
-$option2 = $_POST["option2"];
+$option = $_POST["option"];
 
 $message = str_replace(" ", "%20", $message);
 $message = str_replace("?", "%3F", $message);
-$option1 = str_replace(" ", "%20", $option1);
-$option2 = str_replace(" ", "%20", $option2);
+$option = str_replace(" ", "%20", $option);
+$option = str_replace(" ", "%3F", $option);
 
-if($message && $option1 && $option2)
+$option = implode(",",$option);
+
+if($message && $option)
 {
 $url1 = "https://paypal-payout.herokuapp.com";
 $data = file_get_contents($url1);
 $arr = json_decode($data, true);
 foreach($arr as $child) {
 foreach($child as $child1) {
-$url = 'https://hitman507bot.herokuapp.com/?sender='.$child1["mobile"].'&message='.$message.'&options='.$option1.','.$option2.'';
-file_get_contents($url);	
+$url = 'https://hitman507bot.herokuapp.com/?sender=918050582590&message='.$message.'&options='.$option.'';
+file_get_contents($url);
 }
 }
 echo "Message sent successfully.";
@@ -46,28 +55,38 @@ $data = file_get_contents($url1);
 $arr = json_decode($data, true);
 foreach($arr as $child) {
 foreach($child as $child1) {	
-$url = 'https://hitman507bot.herokuapp.com/?sender='.$child1["mobile"].'&message='.$message.'';
-file_get_contents($url);	
+$url = 'https://hitman507bot.herokuapp.com/?sender=918050582590&message='.$message.'';
+file_get_contents($url);
 }
 }
 echo "Message sent successfully.";
 }	
 ?>
 <br/><br />
+
+<table>
+<tbody>
+<tr class="template" style="display:none;">
+<td>Option(s)</td><td><input type="text" name="option[]" value="" size="28" /></td>
+</tr>
+</tbody>
+</table>
+
 <form method="POST" action="fb_campaign.php" name="form1">
 <table cellspacing="0" cellpadding="10" border="1">
-<tbody>
+<tbody class="fb_content">
 <tr>
 <td>Message</td><td><textarea name="message" rows="5" cols="30" value=""></textarea></td>
 </tr>
 <tr>
-<td>Option1</td><td><input type="text" name="option1" value="" size="28" /></td>
+<td>Option(s)</td><td><input type="text" name="option[]" value="" size="28" /></td>
 </tr>
+</tbody>
+</table>
+<table cellspacing="0" cellpadding="10" border="0" style="border:1px solid #333333; border-top:none;">
+<tbody>
 <tr>
-<td>Option2</td><td><input type="text" name="option2" value="" size="28" /></td>
-</tr>
-<tr>
-<td>&nbsp;</td><td><input type="submit" value="Submit" /></td>
+<td><input type="button" class="add_option" value="Add Option" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="submit" value="Submit" /></td>
 </tr>
 </tbody>
 </table>
