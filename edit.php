944eeff491b023d293f2ed85cd6e4dc038758678
sriharan40 <sriharan40@gmail.com>
2016-11-23 23:53:07 +0530

    Update edit.php

--- edit.php
+++ edit.php
@@ -19,24 +19,22 @@ include("menu.php");
 <body style="margin:0 auto;">
 <script>
 
-    window.fbAsyncInit = function() {
-      FB.init({
-        appId: "312339728800370",
-        xfbml: true,
-        version: "v2.6"
-      });
+window.fbAsyncInit = function() {
+FB.init({
+appId: "312339728800370",
+xfbml: true,
+version: "v2.6"
+});
 
-    };
+};
 
-    (function(d, s, id){
-       var js, fjs = d.getElementsByTagName(s)[0];
-       if (d.getElementById(id)) { return; }
-       js = d.createElement(s); js.id = id;
-       js.src = "//connect.facebook.net/en_US/sdk.js";
-       fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-
-
+(function(d, s, id){
+var js, fjs = d.getElementsByTagName(s)[0];
+if (d.getElementById(id)) { return; }
+js = d.createElement(s); js.id = id;
+js.src = "https://connect.facebook.net/en_US/sdk.js";
+fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
 
 /*FB.Event.subscribe('auth.statusChange', function(response) {
   // do something with response
