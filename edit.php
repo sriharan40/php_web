b4c038b2c56b89684255cd6cab7272654f3a4e9a
sriharan40 <sriharan40@gmail.com>
2016-11-24 00:59:05 +0800

    Update edit.php

--- edit.php
+++ edit.php
@@ -72,7 +72,7 @@ window.fbAsyncInit = function() {
 
 
 <div class="fb-messenger-checkbox"  
-  origin=https://php-web.herokuapp.com
+  origin='https://php-web.herokuapp.com'
   page_id=165157840188738
   messenger_app_id=312339728800370
   user_ref="usernumdummy" 
@@ -120,7 +120,7 @@ window.fbAsyncInit = function() {
 
 </div>
 
-
+	</div>
 <div style="clear:both;"></div>
 </body>
 </html>
