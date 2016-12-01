6566da2d25994c18e83226d0d89e877bb50be716
sriharan40 <sriharan40@gmail.com>
2016-12-02 01:36:23 +0530

    Update index.php

--- index.php
+++ index.php
@@ -57,11 +57,20 @@ $img_url = $child21["img_url"];
 }
 }
 ?>
-Title: <input type="text" name="title" value="<?php echo $title; ?>" /><<br />
-Sub Title: <input type="text" name="sub_title" value="<?php echo $title; ?>" /><<br />
-Image URL: <input type="text" name="img_url" value="<?php echo $title; ?>" /><<br />
-
-<input type="submit" name="submit" value="Update" />
+<form name="form1" method="POST" action="index.php">
+<table cellspacing="0" style="margin-left:0px;" cellpadding="10" >
+<tr>
+<td>Title:</td> <td><input type="text" name="title" value="<?php echo $title; ?>" /></td>
+</tr>
+<tr>
+<td>Sub Title:</td> <td><input type="text" name="sub_title" value="<?php echo $sub_title; ?>" /></td>
+<tr>
+<td>Image URL:</td> <td><input type="text" name="img_url" value="<?php echo $img_url; ?>" /></td>
+<tr>
+<td colspan="2"><input type="submit" name="submit" value="Update" /></td>
+</tr>
+</form>
+</table>
 <br />
 <table cellspacing="0" style="margin-left:0px;" cellpadding="10" >
 <tr>
