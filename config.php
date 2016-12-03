299b04d2c0e907e0bc04b3ef5c73f260b4b29e33
sriharan40 <sriharan40@gmail.com>
2016-12-04 02:05:46 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(0);
-echo $access = $_POST['access']; 
-//if($access == 1){
-//  echo "abfbb042bf354b45965a4a3135f40224";
-//  }
+$access = $_POST['access'];
+if($_POST['access']){
+  echo "abfbb042bf354b45965a4a3135f40224";
+}
 ?>
