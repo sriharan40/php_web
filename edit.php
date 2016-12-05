bb4e9a278d10c4cfc2b0bfc06523d494d8f44f93
sriharan40 <sriharan40@gmail.com>
2016-12-05 09:06:29 +0530

    Update edit.php

--- edit.php
+++ edit.php
@@ -146,7 +146,7 @@ if ( response.event == 'clicked' ) {
 <input size="26" style="width:278px; border-top: 1px solid #575757;" id="input" type="text"> <button id="rec">Speak</button>
 </div>
 
-<iframe width="350" height="430" src="https://php-web.herokuapp.com/iframe.php?access_token=c743619629b2490fab9751dac552094a"></iframe>	
+<iframe width="450" height="530" src="https://php-web.herokuapp.com/iframe.php?access_token=c743619629b2490fab9751dac552094a"></iframe>	
 	
 	</div>
 	
