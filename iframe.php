0dfec0a6b956b68d48b01ae6f9f5879550ab08db
sriharan40 <sriharan40@gmail.com>
2016-12-06 23:20:27 +0530

    Update iframe.php

--- iframe.php
+++ iframe.php
@@ -1,3 +1,4 @@
+<html style="padding:15px;">
 <style>
 body {
     background: #EEE;
@@ -124,4 +125,5 @@ function setResponse(val) {
 <p style="width:100%; padding:10px; padding-left:0px; padding-right:0px; color:#fff; max-width:100%; margin-top:0px;  background-color:#333;">&nbsp;&nbsp;&nbsp;&nbsp;Voice help desk</p>
 <div id="response" style="padding:20px; padding-top:5px;  height:65%; width:93%; overflow:auto;"></div>	
 </div>
-<input size="26" style="width:99%; padding:10px; float:left; border: 1px solid #575757;" id="input" type="text"> <img src="mike.png" width="36" style="margin-left:-40px; cursor:pointer;" id="rec" />
+<input size="30" style="width:99%; padding:10px; float:left; border: 1px solid #575757;" id="input" type="text"> <img src="mike.png" width="36" style="cursor:pointer; position: absolute; bottom: 0px; right: -30px; cursor: pointer; width: 110px;" id="rec" />
+</html>
