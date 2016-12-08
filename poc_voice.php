cf1f0ec964df4fa37a9102508d34724af7f1dcb9
sriharan40 <sriharan40@gmail.com>
2016-12-08 20:00:52 +0530

    Update poc_voice.php

--- poc_voice.php
+++ poc_voice.php
@@ -2,7 +2,7 @@
 session_start();
 ?>
 <html>
-<title>Add Top Offers</title>
+<title>POC Voice</title>
 <script src="jquery-1.12.4.js"></script>
 <script src="script.js"></script> 
 <?php
@@ -14,18 +14,11 @@ if ($_SESSION['login'] == "")
 
 include("menu.php");
 ?>
-
+<body>
+	
 <div class="chat_window" style="margin-left:20px; float:left; display:none;">
 
-<div style="background-color:#ffffff; border:1px solid #aaaaaa; border-bottom:none; width:275px;">			
-<p style="width:255px; padding:10px; color:#fff; margin-top:0px;  background-color:#333;">
-	Voice help desk</p><div id="response" style="padding:20px;padding-top:5px;  height:170px;width:235px;  overflow:auto;"></div>	
-	</div>
-<input size="26" style="width:278px; border-top: 1px solid #575757;" id="input" type="text"> <button id="rec">Speak</button>
-
 <iframe frameborder="0" scrolling="no" width="420" height="390" src="https://php-web.herokuapp.com/iframe.php?access_token=abfbb042bf354b45965a4a3135f40224"></iframe>	
-	
-</div>
 	
 </div>
 
