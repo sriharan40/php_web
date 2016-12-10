df45f426863b96021785c37f64591f9366ac5b59
sriharan40 <sriharan40@gmail.com>
2016-12-10 18:34:28 +0530

    Update amdocs.php

--- amdocs.php
+++ amdocs.php
@@ -1,7 +1,7 @@
-<script type='text/javascript' src='jquery-1.12.4.js'></script>
+<!-- <script type='text/javascript' src='jquery-1.12.4.js'></script> -->
 <script>
-var Credentials = {"user":"Asmsa1","password":"Asmsa1"};
-	$.ajax({
+/* var Credentials = {"user":"Asmsa1","password":"Asmsa1"};
+	$.post({
 		url: "http://ipllin07.amdocs.com/rest/nphproxy.pl/000000A/http/lrt930s150m377:51000/rp-webapp-9-common/Login",
 	    type: "POST",
     	data: JSON.stringify({"Credentials": Credentials}),
@@ -20,5 +20,25 @@ var Credentials = {"user":"Asmsa1","password":"Asmsa1"};
                     alert(e);
                 }
             } 		
-	});
+	}); */
 </script>
+<?php
+$data_string = '{"Credentials":{"user":"Asmsa1","password":"Asmsa1"}}';                                                                  
+                                                                                                               
+$ch = curl_init('http://ipllin07.amdocs.com/rest/nphproxy.pl/000000A/http/lrt930s150m377:51000/rp-webapp-9-common/Login');                                                                      
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);    
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+curl_setopt($ch, CURLOPT_HEADER, true);                                                              
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+    'Content-Type: application/json'                                                                               
+));                                                                                                                   
+         
+//$info = curl_getinfo($ch);
+		 
+echo $result = curl_exec($ch);
+
+//echo $header = substr($result, 0, $info['header_size']);
+
+//echo curl_getinfo($ch, CURLINFO_HTTP_CODE);
+?>
