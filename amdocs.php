b7f78d3b77cbd153ab05afae1e5813aa09bc0581
sriharan40 <sriharan40@gmail.com>
2016-12-10 12:05:01 +0530

    Update amdocs.php

--- amdocs.php
+++ amdocs.php
@@ -1,23 +1,13 @@
 <script src="jquery-1.12.4.js"></script>
 <script>
 	$.ajax({
-
-	POST /rest/nphproxy.pl/000000A/http/lrt930s150m377:51000/rp-webapp-9-common/Login HTTP/1.1
-	Host: ipllin07.amdocs.com
-	Content-Type: application/JSON
-	Cache-Control: no-cache
-
-	{"Credentials":{"user":"Asmsa1","password":"Asmsa1"}}
-
-/*	'type': 'POST',
+		'data': JSON.stringify({"Credentials":{"user":"Asmsa1","password":"Asmsa1"}}),
+	   'type': 'POST',
         'url': "rest/nphproxy.pl/000000A/http/lrt930s150m377:51000/rp-webapp-9-common/Login",
-		//'url': 'https://ipllin07.amdocs.com/',
 		'Accept': 'application/json',
-		//headers: {
-		'Content-Type': 'application/json',
-        'Cache-Control': 'no-cache',
-		//},
-		{"Credentials":{"user":"Asmsa1","password":"Asmsa1"}} */
+		headers: {
+		'contentType': 'application/json'
+		},
 		success: function(data) {
 		alert(data);	
 		}
