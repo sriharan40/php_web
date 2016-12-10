a22510e4d4f9b275abad62b7070653870ac899f1
sriharan40 <sriharan40@gmail.com>
2016-12-10 12:09:26 +0530

    Update amdocs.php

--- amdocs.php
+++ amdocs.php
@@ -1,11 +1,14 @@
 <script src="jquery-1.12.4.js"></script>
 <script>
 	$.ajax({
-		'data': JSON.stringify({"Credentials":{"user":"Asmsa1","password":"Asmsa1"}}),
-	   'type': 'POST',
         'url': "https://ipllin07.amdocs.com/rest/nphproxy.pl/000000A/http/lrt930s150m377:51000/rp-webapp-9-common/Login",
+    	'data': JSON.stringify({"Credentials":{"user":"Asmsa1","password":"Asmsa1"}}),
+	    'type': 'POST',
+	    'crossDomain': true,
+		'dataType': 'jsonp',
 		'Accept': 'application/json',
 		headers: {
+	    'crossDomain': true,			
 		'contentType': 'application/json'
 		},
 		success: function(data) {
