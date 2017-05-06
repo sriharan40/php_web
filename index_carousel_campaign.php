86ae8ed4c863ac4b6319a79d9ea342ca3a4c5d61
sriharan40 <sriharan40@gmail.com>
2017-05-07 01:13:23 +0530

    Add files via upload

--- index_carousel_campaign.php
+++ index_carousel_campaign.php
@@ -121,7 +121,7 @@ echo '<td>&nbsp;&nbsp; <a href="index.php?id='.$child1["id"].'">Delete</a></td>'
 <a href="edit_carousel_campaign.php">Add New</a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-<a href="index.php?notify=1">Notify FB Users</a>
+<a href="index_carousel_campaign.php?notify=1">Notify FB Users</a>
 </div>
 <div style="clear:both;"></div>
 </body>
