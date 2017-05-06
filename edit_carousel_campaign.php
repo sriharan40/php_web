1248a096041b683a032af3fc9a04981b60271885
sriharan40 <sriharan40@gmail.com>
2017-05-06 23:04:05 +0530

    Add files via upload

--- edit_carousel_campaign.php
+++ edit_carousel_campaign.php
@@ -142,19 +142,19 @@ if ( response.event == 'clicked' ) {
 </table>
 </form>
 
-<div class="chat_window" style="margin-left:20px; float:left; display:none;">
+<!-- <div class="chat_window" style="margin-left:20px; float:left; display:none;">
 
-<!--<div style="float:left;">
+<div style="float:left;">
 	<div style="background-color:#ffffff; border:1px solid #aaaaaa; border-bottom:none; width:275px;">			
 <p style="width:255px; padding:10px; color:#fff; margin-top:0px;  background-color:#333;">
 	Voice help desk</p><div id="response" style="padding:20px;padding-top:5px;  height:170px;width:235px;  overflow:auto;"></div>	
 	</div>
 <input size="26" style="width:278px; border-top: 1px solid #575757;" id="input" type="text"> <button id="rec">Speak</button>
-</div>-->
+</div>
 
 <iframe frameborder="0" scrolling="no" width="420" height="390" src="https://php-web.herokuapp.com/iframe.php?access_token=abfbb042bf354b45965a4a3135f40224"></iframe>	
 	
-</div>
+</div> -->
 	
  
 <!-- <div style="margin-left:20px;" class="fb-page" 
